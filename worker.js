d83adf76d8d42b02ef922a95a398efc2b5185f2e
Brent Kimmel <brentkimmel@brentkimmel.com>
2014-10-30 18:45:49 -0400

    sw

--- worker.js
+++ worker.js
@@ -5,9 +5,14 @@
 console.log("SW startup");
 
 var globalvar = 0;
+//globalvar stays in scope through the life of the worker
+
+var installran = "no";
 
 self.addEventListener('install', function(event) {
   console.log("SW installed");
+  //this will only run if the browser has no serviceworkers for this url?
+  installran = "yes";
 });
 
 self.addEventListener('activate', function(event) {
@@ -16,5 +21,6 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   console.log("Globalvar:" + globalvar++);
+  console.log("install ran?: " + installran);
   event.respondWith(new Response("Hello world!" + Math.random()));
 });
