c1e7700e8ebdd5b8afab2f48b20048228e12b89e
Brent Kimmel <brentkimmel@brentkimmel.com>
2014-10-30 19:12:20 -0400

    sw

--- worker.js
+++ worker.js
@@ -9,14 +9,32 @@ var globalvar = 0;
 
 var installran = "no";
 
+function Prom() {
+    var ret = this;
+    var p = new Promise(function(resolve, reject){
+        ret.res = resolve;
+        ret.rej = reject;
+    });
+    this.promise = p;
+}
+
+function someothercrazyfunction(res) {
+  var rand = Math.random * 2000 + 2000;
+  setTimeout(function(){ res('waited ' + rand + ' ms!!'); }, rand)
+}
+
 self.addEventListener('install', function(event) {
   console.log("SW installed");
-  //this will only run if the browser has no serviceworkers for this url?
+  //this will only run if the browser has not seen this version of the SW yet
   installran = "yes";
 });
 
 self.addEventListener('activate', function(event) {
   console.log("SW activated");
+  //this will run when the old version is disposed of and this one is ready to rock
+  var pr = new Prom();
+  event.waitUntil(pr.promise.then(function(v){ console.log('stuff done at activation - resolved with:' + v); }));
+  someothercrazyfunction(pr.res);
 });
 
 self.addEventListener('fetch', function(event) {
