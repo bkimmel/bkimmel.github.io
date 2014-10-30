ddac8cd3ef21a44c11bb85d2462274df77944ed8
Brent Kimmel <brentkimmel@brentkimmel.com>
2014-10-30 17:50:57 -0400

    sw

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ console.log('\'Allo \'Allo!');
 console.log(!!navigator.serviceWorker.register);
 
 navigator.serviceWorker.register('/scripts/worker.js', {
-  scope: '//C:/Users/kimmel_admin/Documents/DevTools/serviceworker/'
+  scope: '/'
 }).then(function(reg) {
   console.log('◕‿◕', reg);
 }, function(err) {
