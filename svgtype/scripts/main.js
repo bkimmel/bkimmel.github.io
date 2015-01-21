5b11e302bc85bf2fd8fe1153dead3333db28845e
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-01-21 10:53:27 -0500

    lt

--- svgtype/scripts/main.js
+++ svgtype/scripts/main.js
@@ -1,12 +1,13 @@
 console.log('\'Allo \'Allo!');
 var gradstate = 0;
 document.getElementById('scrgrad').addEventListener('click', function(){
-  console.log('hover');
+  console.log('click');
   gradstate = !!gradstate ? 0 : 50;
   var gradstop = document.getElementById('myRadialGradient4').querySelector('stop');
   var _start = Date.now(), dur = 1000;
   function stepgrad() {
     if (Date.now() - _start >= dur) {
+	  console.log('complete');
       return gradstop.setAttribute(null, 'offset', gradstate + '%');
     }
     var pct = !!gradstate ? ( ( ( Date.now() - _start ) / dur ) * gradstate ) + '%' : ( gradstate - ( ( ( Date.now() - _start ) / dur ) * gradstate ) ) + '%';
