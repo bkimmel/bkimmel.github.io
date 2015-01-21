b8c1e59449d4f3253ba874d7005d87e7dfda986d
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-01-20 21:26:42 -0500

    lightning talk

--- svgtype/scripts/main.js
+++ svgtype/scripts/main.js
@@ -1,11 +1,1 @@
 console.log('\'Allo \'Allo!');
-
-console.log(!!navigator.serviceWorker.register);
-
-navigator.serviceWorker.register('/worker.js', {
-  scope: '/'
-}).then(function(reg) {
-  console.log('◕‿◕', reg);
-}, function(err) {
-  console.log('ಠ_ಠ', err);
-});
