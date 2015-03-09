c889f10129307e1f03ceb698898dcd615a8d4952
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-03-09 19:28:41 -0400

    sw test

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@ console.log('\'Allo \'Allo!');
 
 console.log(!!navigator.serviceWorker.register);
 
+document.querySelector('button').addEventListener('click', function(e){
+	navigator.serviceWorker.controller.postMessage('hello serviceworker')
+});
+
 var mysw = navigator.serviceWorker.register('/workerb.js', {
   scope: '/'
 }).then(function(reg) {
