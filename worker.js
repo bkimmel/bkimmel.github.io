b6b0f37e07f5114be71e000d386d892f6f65cd3c
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-03-09 18:48:20 -0400

    no-cors change

--- worker.js
+++ worker.js
@@ -49,7 +49,7 @@ self.addEventListener('fetch', function(event) {
 		event.respondWith( new Response('Surprise!!!!') );
 	  }
 	  else if(rando > 7) {
-		event.respondWith( fetch('https://www.google.com') );
+		event.respondWith( fetch('https://www.google.com', {mode: 'no-cors'}) );
 	  }
   }
   
