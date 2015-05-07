ca0f86a64090d0ec905d8b1bcf973b473e3ea6c6
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-05-07 17:03:52 -0400

    .

---
+++ serviceworkerpush/collector.js
@@ -0,0 +1,7 @@
+console.log('Collector started...');
+console.log('in: ' + process.cwd());
+console.log('with: ' + process.argv.slice(2));
+
+function storePushSubscription(pushsubdata, cb_storePushSubscription) {
+	
+}
