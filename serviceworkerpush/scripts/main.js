affa029ce52de24faabc8cd68402b165dfc40d61
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-05-06 22:36:08 -0400

    .

--- serviceworkerpush/scripts/main.js
+++ serviceworkerpush/scripts/main.js
@@ -1,16 +1,66 @@
 console.log('\'Allo \'Allo!');
 
-console.log(!!navigator.serviceWorker.register);
+var isPushEnabled = false;
 
-document.querySelector('button').addEventListener('click', function(e){
-	navigator.serviceWorker.controller.postMessage('hello serviceworker')
-});
+// Once the service worker is registered set the initial state  
+function initialisePushState(reg) {  
+  // Are Notifications supported in the service worker?  
+  if (!(reg['showNotification'])) {  
+    console.warn('Notifications aren\'t supported.');  
+    return;  
+  }
+
+  // Check the current Notification permission.  
+  // If its denied, it's a permanent block until the  
+  // user changes the permission  
+  if (Notification.permission === 'denied') {  
+    console.warn('The user has blocked notifications.');  
+    return;  
+  }
+
+  // Check if push messaging is supported  
+  if (!('PushManager' in window)) {  
+    console.warn('Push messaging isn\'t supported.');  
+    return;  
+  }
+
+  
+  // We need the service worker registration to check for a subscription  
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
+    // Do we already have a push message subscription?  
+    serviceWorkerRegistration.pushManager.getSubscription()  
+      .then(function(subscription) {  
+        // Enable any UI which subscribes / unsubscribes from  
+        // push messages.  
+        var pushButton = document.querySelector('.pushsub');  
+        pushButton.disabled = false;
+
+        if (!subscription) {  
+          // We aren't subscribed to push, so set UI  
+          // to allow the user to enable push  
+          return;  
+        }
+        
+        // Keep your server in sync with the latest subscriptionId
+        sendSubscriptionToServer(subscription);
+
+        // Set your UI to show they have subscribed for  
+        // push messages  
+        pushButton.textContent = 'Disable Push Messages';  
+        isPushEnabled = true;  
+      })  
+      .catch(function(err) {  
+        console.warn('Error during getSubscription()', err);  
+      });  
+  });  
+}
 
 var myswreg;
 navigator.serviceWorker.register('/serviceworkerpush/worker.js', {
   scope: '/serviceworkerpush/'
 }).then(function(reg) {
   console.log('◕‿◕', reg);
+  initialisePushState(reg);
 }, function(err) {
   console.log('ಠ_ಠ', err);
 });
