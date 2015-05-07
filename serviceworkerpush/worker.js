7ec05b0d24f31fc97b9c37f251731bd17695a8c8
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-05-07 09:57:48 -0400

    rick flair

--- serviceworkerpush/worker.js
+++ serviceworkerpush/worker.js
@@ -2,9 +2,9 @@ console.log('Started Push Worker');
 self.addEventListener('push', function(event) {  
   console.log('Received a push message', event);
  
-  var title = 'Yay a message.';  
-  var body = 'We have received a push message.';  
-  var icon = '/images/icon-192x192.png';  
+  var title = 'Woooooo!';  
+  var body = 'It\'s a secret to everybody.';  
+  var icon = 'http://tbo.com/storyimage/TB/20140101/ARTICLE/140109943/EP/1/1/EP-140109943.jpg';  
   var tag = 'simple-push-demo-notification-tag';
 
   event.waitUntil(  
