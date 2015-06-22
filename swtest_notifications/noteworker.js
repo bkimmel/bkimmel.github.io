53243b0c64dbaa897677062f8f2181c3d399d27b
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-06-21 22:41:22 -0400

    .

--- swtest_notifications/noteworker.js
+++ swtest_notifications/noteworker.js
@@ -1,6 +1,12 @@
-self.registration.showNotification('Test Notification', {  
-  body: 'testbody',  
-  icon: 'https://www.google.com/logos/doodles/2015/fathers-day-2015-multiple-countries-5678804118798336-res.png',  
-  tag: 'test-tag' 
-});  
+self.addEventListener('install', function(event) {
+  console.log("SW Alpha installed");
+  //this will only run if the browser has not seen this version of the SW yet
+  self.registration.showNotification('Test Notification', {  
+	  body: 'testbody',  
+	  icon: 'https://www.google.com/logos/doodles/2015/fathers-day-2015-multiple-countries-5678804118798336-res.png',  
+	  tag: 'test-tag' 
+	});  
+});
+
+
       
