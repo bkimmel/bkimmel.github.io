7dc7ff3afdbe0401e0b1d73d70ecc66f9d1d16fd
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-06-21 23:48:04 -0400

    .

--- swtest_notifications/noteworker.js
+++ swtest_notifications/noteworker.js
@@ -13,10 +13,12 @@ self.addEventListener('message', function handlemessage(ev){
 self.addEventListener('install', function(event) {
   console.log("SW Alpha installed");
   //this will only run if the browser has not seen this version of the SW yet
-  event.waitUntil(prom);
+  
 });
 
 self.addEventListener('activate', function(event){
+	event.waitUntil(prom);
+
 	self.registration.showNotification('Chrome Exclusive - Additional 10% Off!', {  
 	  body: 'We lowered the price or something....',  
 	  icon: 'https://rosettastone.okta.com/bc/image/fileStoreRecord?id=fs02cu04ljBTIJDKMMSC',  
