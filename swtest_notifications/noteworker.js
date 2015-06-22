a56b524d64dd63e09d84d0175e58b63b048dff4c
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-06-21 23:50:07 -0400

    .

--- swtest_notifications/noteworker.js
+++ swtest_notifications/noteworker.js
@@ -13,11 +13,11 @@ self.addEventListener('message', function handlemessage(ev){
 self.addEventListener('install', function(event) {
   console.log("SW Alpha installed");
   //this will only run if the browser has not seen this version of the SW yet
-  
+  event.waitUntil(prom);
 });
 
 self.addEventListener('activate', function(event){
-	event.waitUntil(prom);
+	
 
 	self.registration.showNotification('Chrome Exclusive - Additional 10% Off!', {  
 	  body: 'We lowered the price or something....',  
