20d012f4ef807321d43c0bc01648a10feaaa133d
Brent Kimmel <brentkimmel@brentkimmel.com>
2016-03-16 00:17:46 -0400

    .

--- serviceworker_bgs/sw.js
+++ serviceworker_bgs/sw.js
@@ -3,15 +3,19 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('sync', function(syncevent) {
+  console.info('sync activated');
   syncevent.waitUntil(new Promise(function(res, rej){
     var request = indexedDB.open("MyTestDatabase",11);
+    console.info('sync waiting');
     request.onsuccess = function(event) {
       var db = event.target.result;
       var scanner = db.transaction(['pageloads']).objectStore('pageloads').openCursor();
       scanner.onsuccess = function(evt){
           var cursor = evt.target.result;
-          //self.registration.showNotification(JSON.stringify(cursor.value));
-          var i = setInterval(function(){
+          console.info('scan success');
+          self.registration.showNotification(new Date() - (new Date()).setTime( cursor.value ));
+          res();
+          /*var i = setInterval(function(){
             var elapsed = new Date() - (new Date()).setTime( cursor.value );
             if(elapsed >= 20000) {
               self.registration.showNotification('20 seconds');
@@ -19,6 +23,7 @@ self.addEventListener('sync', function(syncevent) {
               res('done');
             }
           }, 10000);
+          */
       }
     }
   }))
