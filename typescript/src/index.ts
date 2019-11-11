84a467fe8e7bdcad9a86a298c0b8a5f3425aeb37
Brent Kimmel <brentkimmel@brentkimmel.com>
2019-11-11 17:01:39 -0500

    function examples

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -21,6 +21,7 @@ type myShape = {
   abc: number
 }
 
+//equivalent to the statement below
 let myArray: Array<myShape> = [
   {
     abc: 1
@@ -47,3 +48,18 @@ let myTuple: [myShape, ...myShape[]] = [
   {abc: 123},
   {abc: 456}
 ]
+
+// Named function
+function greet(name: string) {  return 'hello ' + name}
+
+// Function expression
+let greet2 = function(name: string) {  return 'hello ' + name}
+
+// Arrow function expression
+let greet3 = (name: string) => {  return 'hello ' + name}
+
+// Shorthand arrow function expression
+let greet4 = (name: string) =>  'hello ' + name
+
+// Function constructor (not ypechecked)
+let greet5 = new Function('name', 'return "hello " + name')
