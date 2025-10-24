aa5770f4a1c487244500095f084c693e0655ebdd
bkimmel <brentkimmel@brentkimmel.com>
2025-10-24 13:23:38 -0400

    cleanup

--- weblocks/relay.js
+++
@@ -1,9 +0,0 @@
-const bc = new BroadcastChannel('worker_relay');
-bc.onmessage = (msg)=>{
-    console.log(`Message to worker:`);
-    console.dir(msg);
-    self.postMessage(msg.data);
-}
-self.onmessage = (e)=> {
-    bc.postMessage(e.data);
-}
